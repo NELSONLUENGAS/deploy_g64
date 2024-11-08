cc33a08237918d351de516c1df9238b74ff7921c
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2024-11-07 21:30:05 -0500

    update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const errorMiddleware = require('./middlewares/errorMiddleware.js');
 const routes = require('./routes/routes.js')
+const cors = require('cors')
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://server-g64.onrender.com'],
+}))
 
 // Rutas
 app.use('/', routes);
